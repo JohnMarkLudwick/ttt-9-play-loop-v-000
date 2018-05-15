a4c3602bcc1a89da43c2a5b3555fc67260c13f2d
JohnMark <johnmarkludwick@gmail.com>
2018-05-15 08:13:22 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   turn = 0
   while turn < 9
   puts "turn #{[turn]}"
