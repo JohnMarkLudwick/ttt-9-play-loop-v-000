dfee0e60d46dfc4eba9e894466b6479445401ded
JohnMark <johnmarkludwick@gmail.com>
2018-05-15 08:16:20 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,10 +37,11 @@ end
 
 # Define your play method below
 def play(board)
+  expect(board).to eq(["X","X","X","X","X","X","X","X","X",]
   turn = 0
   while turn < 9
   puts "turn #{[turn]}"
   turn += 1
 end
-board == []
+
 end
