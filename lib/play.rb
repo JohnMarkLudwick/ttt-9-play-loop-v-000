e1f79583a76a2af4375ee4397a53d7efcbe0af18
JohnMark <johnmarkludwick@gmail.com>
2018-05-15 08:18:25 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,10 +37,13 @@ end
 
 # Define your play method below
 def play(board)
-  turn = 0
-  while turn < 9
-  puts "turn #{[turn]}"
-  turn += 1
-end
-
+  turns = 3
+  counter = 0
+  loop do
+    counter = counter + 1
+    turn(board)
+    if counter >= turns
+      puts "You only get #{turns} turns baby!"
+    end
+  end
 end
